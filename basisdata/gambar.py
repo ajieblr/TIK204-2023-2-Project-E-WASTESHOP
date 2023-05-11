b15ebe28673e607c5e6f04e81f592f53449906e9
Aufa Rafiki <aufarafi21@mhs.unsyiah.ac.id>
2023-05-11 11:38:09 +0700

    update

--- basisdata/gambar.py
+++ basisdata/gambar.py
@@ -14,7 +14,38 @@ class Gambar:
         koneksi = sqlite3.connect("EWS.db")
 
         sql = f"""INSERT INTO gambar(username, gambar, nama, harga, deskripsi)
-                VALUES (?, ?, ?);"""
+                VALUES (?, ?, ?, ?, ?);"""
         koneksi.execute(sql, (username, gambar, nama, harga, deskripsi))
         koneksi.commit()
         koneksi.close()
+
+    def ubahBarang(id, gambar, nama, harga, deskripsi):
+        koneksi = sqlite3.connect("EWS.db")
+
+        sql = f"""UPDATE toko
+              SET gambar=?,
+                  nama=?,
+                  harga=?,
+                  deskripsi=?
+              WHERE id=?;"""
+        koneksi.execute(sql, (gambar, nama, harga, deskripsi, id))
+        koneksi.commit()
+        koneksi.close()
+
+    def ambilBarangSatuToko (username):
+        conn = sqlite3.connect('EWS.db')
+        cursor = conn.cursor()
+        cursor.execute('SELECT gambar, nama, harga, deskripsi, id FROM gambar WHERE username = ?', (username,))
+        rows = cursor.fetchall()
+        conn.close()
+
+        return rows
+    
+    def ambilBarangTokoOrang (username):
+        conn = sqlite3.connect('EWS.db')
+        cursor = conn.cursor()
+        cursor.execute('SELECT gambar, nama, harga, deskripsi, id FROM gambar WHERE username != ?', (username,))
+        rows = cursor.fetchall()
+        conn.close()
+
+        return rows
