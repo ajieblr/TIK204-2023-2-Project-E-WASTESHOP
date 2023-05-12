0b1c059f69cdd71dd77a93745dbc59df5f99bf85
Alya Irzan <alya_ir@mhs.unsyiah.ac.id>
2023-05-12 11:39:55 +0700

    Auto stash before merge of "main" and "origin/main"

--- basisdata/__init__.py
+++ basisdata/__init__.py
@@ -1,6 +1,14 @@
+<<<<<<< Updated upstream
 from .basisdata import *
 from .user import *
 from .gambar import *
 
 buatTabelUser()
+=======
+from .basisdata import *
+from .user import *
+from .gambar import *
+
+buatTabelUser()
+>>>>>>> Stashed changes
 buatTabelGambar()
--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+<<<<<<< Updated upstream
 from basisdata import *
 
 a = Gambar.ambilBarangSatuToko('2104111010010')
 
+=======
+from basisdata import *
+
+a = Gambar.ambilBarangSatuToko('2104111010010')
+
+>>>>>>> Stashed changes
 print(a[1])
